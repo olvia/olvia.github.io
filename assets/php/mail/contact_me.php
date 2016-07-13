7897ea92dc2694948f5ae2bc24377aeab4cbd76a
Olvia <Lyolka05@mail.ru>
2016-07-13 10:31:04 +0300

    chanfe form

--- assets/php/mail/contact_me.php
+++ assets/php/mail/contact_me.php
@@ -1,20 +1,24 @@
-<?
-if((isset($_POST['name'])&&$_POST['name']!="")&&(isset($_POST['email'])&&$_POST['email']!="")&&(isset($_POST['message'])&&$_POST['message']!="")){ //Проверка отправилось ли наше поля name и не пустые ли они
-        $to = 'for.me.chova@gmail.com'; //Почта получателя, через запятую можно указать сколько угодно адресов
-        $subject = 'Обратный звонок'; //Загаловок сообщения
-        $message = '
-                <html>
-                    <head>
-                        <title>'.$subject.'</title>
-                    </head>
-                    <body>
-                        <p>Имя: '.$_POST['name'].'</p>
-                        <p>Телефон: '.$_POST['email'].'</p>    
-                        <p>Телефон: '.$_POST['message'].'</p>                      
-                    </body>
-                </html>'; //Текст нащего сообщения можно использовать HTML теги
-        $headers  = "Content-type: text/html; charset=utf-8 \r\n"; //Кодировка письма
-        $headers .= "From: Отправитель <from@example.com>\r\n"; //Наименование и почта отправителя
-        mail($to, $subject, $message, $headers); //Отправка письма с помощью функции mail
-}
+<?php
+ini_set('display_errors', 'On');
+error_reporting(E_ALL);
+
+require 'class.phpmailer.php';
+
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+
+$mail = new PHPMailer;
+
+$mail->From = 'test@test.com';
+$mail->FromName = 'Testname';
+$mail->AddAddress('for.me.chova@gmail.com');
+
+$mail->WordWrap = 50;
+$mail->IsHTML(true);
+
+$mail->Body = "Name: $name";
+
+$mail->Send();
 ?>
