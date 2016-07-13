c33a3831e93872e2695beb514561a8287dea2878
Olvia <Lyolka05@mail.ru>
2016-07-13 14:00:50 +0300

    change timeline

--- assets/php/mail/contact_me.php
+++ assets/php/mail/contact_me.php
@@ -1,24 +1,20 @@
-<?php
-ini_set('display_errors', 'On');
-error_reporting(E_ALL);
-
-require 'class.phpmailer.php';
-
-
-$name = $_POST['name'];
-$email = $_POST['email'];
-$message = $_POST['message'];
-
-$mail = new PHPMailer;
-
-$mail->From = 'test@test.com';
-$mail->FromName = 'Testname';
-$mail->AddAddress('for.me.chova@gmail.com');
-
-$mail->WordWrap = 50;
-$mail->IsHTML(true);
-
-$mail->Body = "Name: $name";
-
-$mail->Send();
+<?
+if((isset($_POST['name'])&&$_POST['name']!="")&&(isset($_POST['email'])&&$_POST['email']!="")&&(isset($_POST['message'])&&$_POST['message']!="")){ //Проверка отправилось ли наше поля name и не пустые ли они
+        $to = 'for.me.chova@gmail.com'; 
+        $subject = 'Обратный звонок'; 
+        $message = '
+                <html>
+                    <head>
+                        <title>'.$subject.'</title>
+                    </head>
+                    <body>
+                        <p>Имя: '.$_POST['name'].'</p>
+                        <p>Телефон: '.$_POST['email'].'</p>    
+                        <p>Телефон: '.$_POST['message'].'</p>                      
+                    </body>
+                </html>'; /
+        $headers  = "Content-type: text/html; charset=utf-8 \r\n"; 
+        $headers .= "From: Отправитель <from@example.com>\r\n"; 
+        mail($to, $subject, $message, $headers); 
+}
 ?>
